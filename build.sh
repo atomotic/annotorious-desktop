4736f9fb1ee4293baade1d479ccf80358cb3ac73
raffaele messuti <raffaele@atomotic.com>
2020-07-16 20:58:46 +0200

    macos minimum version

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd public
 yarn
 cp node_modules/openseadragon/build/openseadragon/openseadragon.min.js js/
 cp node_modules/openseadragon/build/openseadragon/openseadragon.min.js.map js/
-cp -av node_modules/openseadragon/build/openseadragon/images .
+cp -a node_modules/openseadragon/build/openseadragon/images .
 cp node_modules/@recogito/annotorious-openseadragon/dist/openseadragon-annotorious.min.js js/
 cp node_modules/@recogito/annotorious-openseadragon/dist/openseadragon-annotorious.min.js.map js/
 rm -fr node_modules
