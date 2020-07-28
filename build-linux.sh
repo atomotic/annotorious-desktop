e7bf562a3a929e67cf321422c5d3f98d574086c0
raffaele messuti <raffaele@atomotic.com>
2020-07-28 08:41:30 +0200

    linux build

---
+++ build-linux.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cd public
+yarn
+cp node_modules/openseadragon/build/openseadragon/openseadragon.min.js js/
+cp node_modules/openseadragon/build/openseadragon/openseadragon.min.js.map js/
+cp -a node_modules/openseadragon/build/openseadragon/images .
+cp node_modules/@recogito/annotorious-openseadragon/dist/openseadragon-annotorious.min.js js/
+cp node_modules/@recogito/annotorious-openseadragon/dist/openseadragon-annotorious.min.js.map js/
+rm -fr node_modules
+
+cd ..
+statik -src=./public
+
+go build
