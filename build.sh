93ee5271e947b26d58072624754699e0316e596b
raffaele messuti <raffaele@atomotic.com>
2020-11-22 18:37:05 +0100

    updated build scripts to copy css

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ cp node_modules/openseadragon/build/openseadragon/openseadragon.min.js.map js/
 cp -a node_modules/openseadragon/build/openseadragon/images .
 cp node_modules/@recogito/annotorious-openseadragon/dist/openseadragon-annotorious.min.js js/
 cp node_modules/@recogito/annotorious-openseadragon/dist/openseadragon-annotorious.min.js.map js/
+cp node_modules/@recogito/annotorious-openseadragon/dist/annotorious.min.css css/
+cp node_modules/@recogito/annotorious-openseadragon/dist/annotorious.min.css.map css/
 rm -fr node_modules
 
 cd ..
